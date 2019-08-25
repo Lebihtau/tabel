f24a8478d2f6a29273199f6330aeb07b9efb6f1a
Ishad Nasoha <32013784+Lebihtau@users.noreply.github.com>
2019-08-25 14:54:45 +0700

    Update tabletop-feed.js

--- tabletop-feed.js
+++ tabletop-feed.js
@@ -48,7 +48,7 @@ function writeTableWith(dataSource){
         "aoColumns": createTableColumns(),
         "fnRowCallback": function(nRow, aData, iDisplayIndex) {
             console.log(aData);
-            $("td:eq(2)", nRow).html("<a href='" + aData.download + "'>Download</a>");
+            $("td:eq(2)", nRow).html("<a href='" + aData.name + "'></a>");
             return nRow;
         },
         "oLanguage": {
