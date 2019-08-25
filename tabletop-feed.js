eb8a79cf6e91be8b6cedcde82c17a13610b18320
Ishad Nasoha <32013784+Lebihtau@users.noreply.github.com>
2019-08-25 13:13:54 +0700

    Update tabletop-feed.js

--- tabletop-feed.js
+++ tabletop-feed.js
@@ -48,7 +48,7 @@ function writeTableWith(dataSource){
         "aoColumns": createTableColumns(),
         "fnRowCallback": function(nRow, aData, iDisplayIndex) {
             console.log(aData);
-            $("td:eq(2)", nRow).html("<a href='http://" + aData.download + "'></a>");
+            $("td:eq(2)", nRow).html("<a href='http://" + aData.download + "'>Download</a>");
             return nRow;
         },
         "oLanguage": {
@@ -60,12 +60,4 @@ function writeTableWith(dataSource){
 
 //define two custom functions (asc and desc) for string sorting
 
-jQuery.fn.dataTableExt.oSort["string-case-asc"]  = function(x,y) {
 
-	return ((x < y) ? -1 : ((x > y) ?  0 : 0));};
-
-jQuery.fn.dataTableExt.oSort["string-case-desc"] = function(x,y) {
-
-	return ((x < y) ?  1 : ((x > y) ? -1 : 0));
-
-};
