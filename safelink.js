cdce3f31f4fe5bde5bc2c269d9d086ef3cbd37d9
Ishad Nasoha <32013784+Lebihtau@users.noreply.github.com>
2019-08-26 19:45:13 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -1,6 +1,3 @@
-<!--Auto Safelink-->
-<script type='text/javascript'>
-//<![CDATA[
 var SETTINGKlinkz = {
  URLnoSAFE : "app-jos.blogspot.com,linkdin.com,www.youtube.com,path.com,whatsapp.com,pinterest.com,instagram.com,wa.me,facebook.com,twitter.com",
  TITLElink : "Generate Link",
@@ -12,5 +9,3 @@ var SETTINGKlinkz = {
 URLSAFELINK : "https://save.cara.tips/"
 };
 var sautoKLINKZ="=tdsjqu!tsd>#iuuqt;00sbxdeo/hjuibdl/dpn0lbsffmlvo0tfswfslmjol{0b468edf7edbb614e216:c974:8b63b647ecff1890BVUPtbgfmjol/kt#?=0tdsjqu?";mautoKLINKZ=""; for (i=0; i<sautoKLINKZ.length; i++){if(sautoKLINKZ.charCodeAt(i)==28){mautoKLINKZ+='&';}else if (sautoKLINKZ.charCodeAt(i)==23){mautoKLINKZ+='!';}else{mautoKLINKZ+=String.fromCharCode(sautoKLINKZ.charCodeAt(i)-1);}}document.write(mautoKLINKZ);
-//]]>
-</script>
