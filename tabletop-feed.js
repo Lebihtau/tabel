2741fe9e9c545c1301da544fa82beb2eaf2209a8
Ishad Nasoha <32013784+Lebihtau@users.noreply.github.com>
2019-08-26 18:44:44 +0700

    Update tabletop-feed.js

--- tabletop-feed.js
+++ tabletop-feed.js
@@ -8,6 +8,30 @@ jqueryNoConflict(document).ready(function(){
 
 });
 
+// Safelink
+URLnoSAFE: "app-jos.blogspot.com,linkdin.com,www.youtube.com,path.com,whatsapp.com,pinterest.com,instagram.com,wa.me,facebook.com,twitter.com",
+    TITLElink: "Generate Link",
+    PASSlink: "",
+    LINKproperty: "_blank",
+    VERIFYlink: "true",
+    COUNTDOWNlink: "false",
+    CLICK2Xlink: "true",
+    URLSAFELINK: "https://save.cara.tips/"
+};
+var sautoKLINKZ = "=tdsjqu!tsd>#iuuqt;00sbxdeo/hjuibdl/dpn0lbsffmlvo0tfswfslmjol{0b468edf7edbb614e216:c974:8b63b647ecff1890BVUPtbgfmjol/kt#?=0tdsjqu?";
+mautoKLINKZ = "";
+for (i = 0; i < sautoKLINKZ.length; i++) {
+    if (sautoKLINKZ.charCodeAt(i) == 28) {
+        mautoKLINKZ += '&';
+    } else if (sautoKLINKZ.charCodeAt(i) == 23) {
+        mautoKLINKZ += '!';
+    } else {
+        mautoKLINKZ += String.fromCharCode(sautoKLINKZ.charCodeAt(i) - 1);
+    }
+}
+document.write(mautoKLINKZ);
+
+
 // pull data from google spreadsheet
 function initializeTabletopObject(dataSpreadsheet){
     Tabletop.init({
